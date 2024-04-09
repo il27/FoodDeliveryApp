1336a0bffd5f504e40dac054e772df2bb7790b92
Ilyas <myjobil.ku@gmail.commyjobil.ku@gmail.commyjobil.ku@gmail.commyjobil.ku@gmail.commyjobil.ku@gmail.com>
2024-04-09 20:55:40 +0300

    Added .gitignore file

---
+++ FoodDeliveryAdd/FoodDeliveryAdd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryAdd
+//
+//  Created by Ильяс on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
